c26e95999930be469091b1e060705285f513c122
Sergey Seroukhov <seroukhov@gmail.com>
2020-06-05 14:47:41 -0700

    Added reviews microservice

--- workspace.conf.ps1
+++ workspace.conf.ps1
@@ -6,5 +6,8 @@ $VersionControlRepos = @(
 	"https://github.com/pip-services-ecommerce/pip-services-invoices-node.git"
 	"https://github.com/pip-services-ecommerce/pip-clients-invoices-node.git"
 
+	"https://github.com/pip-services-ecommerce/pip-services-reviews-dart.git"
+	"https://github.com/pip-services-ecommerce/pip-clients-reviews-dart.git"
+
 	"https://github.com/pip-services-ecommerce/pip-facade-ecommerce-node.git"
 )
