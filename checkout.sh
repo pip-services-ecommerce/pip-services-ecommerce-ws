159e30431be86b651202b02e6661b63bf788100d
Sergey Seroukhov <seroukhov@gmail.com>
2020-07-01 21:54:30 -0700

    Added payroll checks microservice

--- checkout.sh
+++ checkout.sh
@@ -7,6 +7,7 @@ git clone git@github.com:pip-services-ecommerce/pip-services-paymentmethods-node
 git clone git@github.com:pip-services-ecommerce/pip-services-reviews-node.git
 git clone git@github.com:pip-services-ecommerce/pip-services-reviews-dart.git
 git clone git@github.com:pip-services-ecommerce/pip-services-purchaseorders-node.git
+git clone git@github.com:pip-services-ecommerce/pip-services-payrollchecks-node.git
 git clone git@github.com:pip-services-ecommerce/pip-services-payments-node.git
 
 git clone git@github.com:pip-services-ecommerce/pip-clients-creditcards-node.git
@@ -14,4 +15,5 @@ git clone git@github.com:pip-services-ecommerce/pip-clients-paymentmethods-node.
 git clone git@github.com:pip-services-ecommerce/pip-clients-reviews-node.git
 git clone git@github.com:pip-services-ecommerce/pip-clients-reviews-dart.git
 git clone git@github.com:pip-services-ecommerce/pip-clients-purchaseorders-node.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-payrollchecks-node.git
 git clone git@github.com:pip-services-ecommerce/pip-clients-payments-node.git
