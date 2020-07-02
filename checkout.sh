0c5f83fc56202b69b027c5c632fa76c3ed073e73
Sergey Seroukhov <seroukhov@gmail.com>
2020-07-01 21:53:47 -0700

    Added new microservices

---
+++ checkout.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git clone git@github.com:pip-services-ecommerce/pip-facade-ecommerce-node.git
+
+git clone git@github.com:pip-services-ecommerce/pip-services-creditcards-node.git
+git clone git@github.com:pip-services-ecommerce/pip-services-paymentmethods-node.git
+git clone git@github.com:pip-services-ecommerce/pip-services-reviews-node.git
+git clone git@github.com:pip-services-ecommerce/pip-services-reviews-dart.git
+git clone git@github.com:pip-services-ecommerce/pip-services-purchaseorders-node.git
+git clone git@github.com:pip-services-ecommerce/pip-services-payments-node.git
+
+git clone git@github.com:pip-services-ecommerce/pip-clients-creditcards-node.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-paymentmethods-node.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-reviews-node.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-reviews-dart.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-purchaseorders-node.git
+git clone git@github.com:pip-services-ecommerce/pip-clients-payments-node.git
